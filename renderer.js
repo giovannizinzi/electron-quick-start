21e57701ff21c2f093de7639600fbb3d05e88c7c
giovannizinzi <49014225+giovannizinzi@users.noreply.github.com>
2021-10-12 02:37:16 -0500

    another change

--- renderer.js
+++ renderer.js
@@ -4,10 +4,49 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
-const { BrowserWindow } = require('@electron/remote')
+const {ipcRenderer} = require('electron')
 const path = require('path')
 
-const charles = document.getElementById('charleslamannalisting')
+const charles = document.getElementById("charleslamannalisting")
+
 charles.addEventListener('click', (event) => {
-    document.getElementById('sync-reply').innerHTML = message
-})
+    charles.innerHTML += `<span class="activeSpeaker" id=""></span>`
+});
+
+const jason = document.getElementById("jasonzanderlisting")
+
+jason.addEventListener('click', (event) => {
+    jason.innerHTML += `<span class="activeSpeaker" id=""></span>`
+});
+
+const phil = document.getElementById("philspencerlisting")
+
+phil.addEventListener('click', (event) => {
+    phil.innerHTML += `<span class="activeSpeaker" id=""></span>`
+});
+
+function createRipple(event) {
+    const button = event.currentTarget;
+  
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+  
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+    circle.classList.add("ripple");
+  
+    const ripple = button.getElementsByClassName("ripple")[0];
+  
+    if (ripple) {
+      ripple.remove();
+    }
+  
+    button.appendChild(circle);
+  }
+  
+  const buttons = document.getElementsByTagName("button");
+  for (const button of buttons) {
+    button.addEventListener("click", createRipple);
+  }
