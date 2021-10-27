e77754e6573eceaada63dbf61053c2f1a3942bdd
giovannizinzi <49014225+giovannizinzi@users.noreply.github.com>
2021-10-27 00:01:46 -0500

    these are the changes

--- renderer.js
+++ renderer.js
@@ -11,15 +11,20 @@ let {PythonShell} = require('python-shell')
 const http = require('http')
 const fs = require('fs');
 const spawn = require("child_process").spawn;
+const { SSL_OP_EPHEMERAL_RSA } = require('constants')
+const { waitForDebugger } = require('inspector')
+const { 
+  v1: uuidv1,
+  v4: uuidv4,
+} = require('uuid');
+const { create } = require('domain')
 
 const anuj = document.getElementById("anujsaharanlisting")
 const charles = document.getElementById("charleslamannalisting")
 const gio = document.getElementById("giovannizinzilisting")
 const jason = document.getElementById("jasonzanderlisting")
 const phil = document.getElementById("philspencerlisting")
-const right = document.getElementById("right")
-
-
+const audio = document.querySelector('.audioSection')
 const button = document.getElementById("button");
 
 
@@ -35,163 +40,71 @@ document.getElementById('audioDownload').onclick = function() {
 
 document.getElementById('audioDownload2').onclick = function() {
   document.getElementById('my_file2').click();
-  //After input uploaded, try to run some python commands? Will this work? I.e. can I get this to input into the model?
-  
+  //After input uploaded, try to run some python commands? Maybe send request to flask server too
 };
 
 charles.addEventListener('click', (event) => {
-    if(document.getElementById("activeSpeakerJason")) {
-      document.getElementById("activeSpeakerJason").remove();
-    }
-    if(document.getElementById("activeSpeakerPhil")) {
-      document.getElementById("activeSpeakerPhil").remove();
-    }
-    if(document.getElementById("activeSpeakerGio")) {
-      document.getElementById("activeSpeakerGio").remove();
-    }
-    if(document.getElementById("activeSpeakerAnuj")) {
-      document.getElementById("activeSpeakerAnuj").remove();
-    }
+
+  if(document.querySelector('.activeSpeaker')) {
+    removeActiveSpeaker()
+  }
 
     charles.innerHTML += `<span class="activeSpeaker" id="activeSpeakerCharles"></span>`
 
+    text = document.getElementById('textInput').value
+    charlesModel = ""
+    createAudio(charlesModel, text)
 });
 
 jason.addEventListener('click', (event) => {
-    if(document.getElementById("activeSpeakerCharles")) {
-      document.getElementById("activeSpeakerCharles").remove();
-    }
-    if(document.getElementById("activeSpeakerPhil")) {
-      document.getElementById("activeSpeakerPhil").remove();
-    }
-    if(document.getElementById("activeSpeakerGio")) {
-      document.getElementById("activeSpeakerGio").remove();
-    }
-    if(document.getElementById("activeSpeakerAnuj")) {
-      document.getElementById("activeSpeakerAnuj").remove();
-    }
+
+  if(document.querySelector('.activeSpeaker')) {
+    removeActiveSpeaker()
+  }
 
     jason.innerHTML += `<span class="activeSpeaker" id="activeSpeakerJason"></span>`
+
+    text = document.getElementById('textInput').value
+    jasonModel = ""
+    createAudio(jasonModel, text)
 });
 
 phil.addEventListener('click', (event) => {
-    if(document.getElementById("activeSpeakerJason")) {
-      document.getElementById("activeSpeakerJason").remove();
+    if(document.querySelector('.activeSpeaker')) {
+      removeActiveSpeaker()
     }
-    if(document.getElementById("activeSpeakerCharles")) {
-      document.getElementById("activeSpeakerCharles").remove();
-    }
-    if(document.getElementById("activeSpeakerGio")) {
-      document.getElementById("activeSpeakerGio").remove();
-    }
-    if(document.getElementById("activeSpeakerAnuj")) {
-      document.getElementById("activeSpeakerAnuj").remove();
-    }
-
     phil.innerHTML += `<span class="activeSpeaker" id="activeSpeakerPhil"></span>`
 
+    text = document.getElementById('textInput').value
+    philModel = "1k-G8UqLyoObEoyHM2eNEXt8Z8YgDE2yz"
+    createAudio(philModel, text)
 });
 
 gio.addEventListener('click', (event) => {
-  if(document.getElementById("activeSpeakerJason")) {
-    document.getElementById("activeSpeakerJason").remove();
-  }
-  if(document.getElementById("activeSpeakerPhil")) {
-    document.getElementById("activeSpeakerPhil").remove();
-  }
-  if(document.getElementById("activeSpeakerCharles")) {
-    document.getElementById("activeSpeakerCharles").remove();
-  }
-  if(document.getElementById("activeSpeakerAnuj")) {
-    document.getElementById("activeSpeakerAnuj").remove();
+
+  if(document.querySelector('.activeSpeaker')) {
+    removeActiveSpeaker()
   }
 
   gio.innerHTML += `<span class="activeSpeaker" id="activeSpeakerGio"></span>`
 
   text = document.getElementById('textInput').value
-  console.log(text)
+  gioModel = "1D0z716mepv9-CAxrPR0-4aO074ONMtwg"
+  createAudio(gioModel, text)
 
-  const data = JSON.stringify({
-    transcript : text,
-    model : "1D0z716mepv9-CAxrPR0-4aO074ONMtwg"})
-  
-  const options = {
-    hostname: '192.168.1.23',
-    port: 8080,
-    path: '/talknet',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length
-    }
-  }
-  
-  const req = http.request(options, res => {
-    var str = ''
-    console.log(`statusCode: ${res.statusCode}`)
-    res.on('data', function (body) {
-      str += body;
-    })
-
-    res.on('end', function () {
-      console.log('writing')
-      fs.writeFile('textForPython6.txt', str, err => {
-        if (err) {
-          console.error(err)
-          return
-        }
-        //file written successfully
-      })
-      const pythonProcess = spawn('python',["convertToFile.py"]);
-      // right.innerHTML +=
-      // `
-      // <audio controls>
-      //   <source src="bb560.wav" type="audio/wav">
-      //   Your browser does not support the audio element.
-      // </audio>
-      // `
-    });
-
-  })
-  
-  req.on('error', error => {
-    console.error(error)
-  })
-  
-  req.write(data)
-  req.end()
 });
-
 
 anuj.addEventListener('click', (event) => {
 
-text = document.getElementById('textInput').value
-
-//Let's automatically send this to a python script. That makes the most sense? Local controllable talknet 
-if(text) {
-  console.log(text)
-  var bb = new Blob([text], { type: 'text/plain' });
-  var a = document.createElement('a');
-  a.download = 'download.txt';
-  a.href = URL.createObjectURL(bb);
-  a.click();
-}
-
-//Need to change this to something more dynamic. Get a list of all speakers available?
-  if(document.getElementById("activeSpeakerJason")) {
-    document.getElementById("activeSpeakerJason").remove();
-  }
-  if(document.getElementById("activeSpeakerPhil")) {
-    document.getElementById("activeSpeakerPhil").remove();
-  }
-  if(document.getElementById("activeSpeakerCharles")) {
-    document.getElementById("activeSpeakerCharles").remove();
-  }
-  if(document.getElementById("activeSpeakerGio")) {
-    document.getElementById("activeSpeakerGio").remove();
+  if(document.querySelector('.activeSpeaker')) {
+    removeActiveSpeaker()
   }
 
   anuj.innerHTML += `<span class="activeSpeaker" id="activeSpeakerAnuj"></span>`
+
+  text = document.getElementById('textInput').value
+  anujModel = ""
+  createAudio(anujModel, text)
 
 });
 
@@ -205,6 +118,15 @@ document.querySelector('.close').addEventListener("click", function() {
   document.querySelector('.bg-modal').style.display = "none";
   document.querySelector('#container').style.display = "grid";
 });
+
+// if(text) {
+//   console.log(text)
+//   var bb = new Blob([text], { type: 'text/plain' });
+//   var a = document.createElement('a');
+//   a.download = 'download.txt';
+//   a.href = URL.createObjectURL(bb);
+//   a.click();
+// }
 
 function createRipple(event) {
     const button = event.currentTarget;
@@ -225,9 +147,73 @@ function createRipple(event) {
     }
   
     button.appendChild(circle);
-  }
+}
+
+function removeActiveSpeaker() {
+  var elements = document.querySelectorAll('.activeSpeaker')
+      for(var i = 0; i < elements.length; i++){
+        elements[i].remove()
+      }
+}
+
+function createAudio(model, text) {
+  console.log(text)
+
+  const data = JSON.stringify({
+    transcript : text,
+    model : model})
   
-  const buttons = document.getElementsByTagName("button");
-  for (const button of buttons) {
-    button.addEventListener("click", createRipple);
+  const options = {
+    hostname: '192.168.1.23',
+    port: 8080,
+    path: '/talknet',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
   }
+
+  const req = http.request(options, res => {
+    var str = ''
+    var uuid = uuidv4()
+    console.log(`statusCode: ${res.statusCode}`)
+    res.on('data', function (body) {
+      str += body;
+    })
+
+    res.on('end', function () {
+      console.log('writing')
+      fs.writeFile('textForPython6.txt', str, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        //file written successfully
+      })
+      const pythonProcess = spawn('python',["convertToFile.py", uuid]);
+      pythonProcess.on('exit', function() {
+      
+      audioID = document.getElementById('player')
+      audioID.removeAttribute('src')
+      audioID.pause()
+      audioID.src = uuid + ".wav"
+      audioID.load()
+      audioID.play()
+      })
+    });
+
+  })
+  
+  req.on('error', error => {
+    console.error(error)
+  })
+  
+  req.write(data)
+  req.end()
+}
+  
+const buttons = document.getElementsByTagName("button");
+for (const button of buttons) {
+  button.addEventListener("click", createRipple);
+}
